4df1434e21f0a6010fb61ff8437a3a82a04e4c9a
Muhammad Talha <talha18513@gmail.com>
2025-08-28 06:02:37 -0700

    In progress

--- docs/javascript/navbar.js
+++ docs/javascript/navbar.js
@@ -58,7 +58,7 @@ dynamic_navbar.innerHTML = `
                     <li><a href="/index.html"
                             class="hover:opacity-60 mobileMenuCloseBtn tracking-wide  transition-all duration-500 text-nowrap">Home</a>
                     </li>
-                    <li><a href="/food.html"
+                    <li><a href="/drink.html"
                     class="hover:opacity-60 mobileMenuCloseBtn tracking-wide text-nowrap transition-all duration-500 text-nowrap">Menu</a>
                     </li>
                     <li><a href="/visit.html"
